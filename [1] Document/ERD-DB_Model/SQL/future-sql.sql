a761ccd89d6ff99be3842cedf8f1cb12df476804
satyadara <satyadara69@gmail.com>
2017-10-15 10:47:00 +0700

    DB

--- [1] Document/ERD-DB_Model/SQL/future-sql.sql
+++ [1] Document/ERD-DB_Model/SQL/future-sql.sql
@@ -1,58 +1,20 @@
 /*==============================================================*/
 /* DBMS name:      PostgreSQL 9.x                               */
-/* Created on:     08/10/2017 08.02.31                          */
+/* Created on:     15/10/2017 10.28.26                          */
 /*==============================================================*/
-
-
-drop index ADD_DISCOUNT_FK;
-
-drop index DISCOUNT_PK;
-
-drop table DISCOUNT;
-
-drop index EMPLOYEE_PK;
-
-drop table EMPLOYEE;
-
-drop index ADD_ITEM_FK;
-
-drop index ITEM_PK;
-
-drop table ITEM;
-
-drop index ORDERHASORDERLINE_FK;
-
-drop index ITEMHASORDERLINE_FK;
-
-drop index ORDERLINE_PK;
-
-drop table ORDERLINE;
-
-drop index HAS_OUTCOME_FK;
-
-drop index OUTCOME_PK;
-
-drop table OUTCOME;
-
-drop index ADD_ORDER_FK;
-
-drop index HAS_FK;
-
-drop index TRANSACTION_PK;
-
-drop table TRANSACTION;
 
 /*==============================================================*/
 /* Table: DISCOUNT                                              */
 /*==============================================================*/
+create sequence disc START 1001;
 create table DISCOUNT (
-   ID_DISC              INT4                 not null,
-   EMP_ID               INT4                 not null,
-   NAME_DISC            VARCHAR(255)         null,
-   PERCENTAGE           FLOAT8               null,
-   START_DATE           DATE                 null,
-   END_DATE             DATE                 null,
-   STATUS_DISC          VARCHAR(15)          null,
+   ID_DISC              VARCHAR(10)	     DEFAULT 'DISC-' || nextval('disc'),
+   ID_EMP               VARCHAR(10)          not null,
+   NAME_DISC            VARCHAR(254)         not null,
+   PERCENTAGE           FLOAT8               not null,
+   START_DATE           DATE                 not null,
+   END_DATE             DATE                 not null,
+   STATUS_DISC          VARCHAR(15)          not null,
    constraint PK_DISCOUNT primary key (ID_DISC)
 );
 
@@ -67,45 +29,56 @@ ID_DISC
 /* Index: ADD_DISCOUNT_FK                                       */
 /*==============================================================*/
 create  index ADD_DISCOUNT_FK on DISCOUNT (
-EMP_ID
+ID_EMP
 );
 
 /*==============================================================*/
 /* Table: EMPLOYEE                                              */
 /*==============================================================*/
+create sequence emp START 1001;
+
 create table EMPLOYEE (
-   EMP_ID               INT4                 not null,
-   USERNAME             VARCHAR(15)          null,
-   PASSWORD             VARCHAR(64)          null,
-   ROLE                 VARCHAR(20)          null,
-   NOKTP                VARCHAR(35)          null,
-   FULL_NAME            VARCHAR(60)          null,
-   ADDRESS              VARCHAR(256)         null,
-   GENDER               CHAR(1)              null,
-   TELEPHONE            VARCHAR(15)          null,
-   STATUS_EMPLOYEE      VARCHAR(15)          null,
-   constraint PK_EMPLOYEE primary key (EMP_ID)
+   ID_EMP               VARCHAR(10)          DEFAULT 'EMP-' || nextval('emp'),
+   ID_ROLE              VARCHAR(10)          not null,
+   USERNAME             VARCHAR(15)          not null,
+   PASSWORD             VARCHAR(64)          not null,
+   NOKTP                VARCHAR(35)          not null,
+   FULL_NAME            VARCHAR(60)          not null,
+   ADDRESS              VARCHAR(256)         not null,
+   GENDER               CHAR(1)              not null,
+   TELEPHONE            VARCHAR(15)          not null,
+   STATUS_EMPLOYEE      VARCHAR(15)          not null,
+   constraint PK_EMPLOYEE primary key (ID_EMP)
 );
 
 /*==============================================================*/
 /* Index: EMPLOYEE_PK                                           */
 /*==============================================================*/
 create unique index EMPLOYEE_PK on EMPLOYEE (
-EMP_ID
+ID_EMP
+);
+
+/*==============================================================*/
+/* Index: EMPHASROLE_FK                                         */
+/*==============================================================*/
+create  index EMPHASROLE_FK on EMPLOYEE (
+ID_ROLE
 );
 
 /*==============================================================*/
 /* Table: ITEM                                                  */
 /*==============================================================*/
+create sequence item START 1001;
+
 create table ITEM (
-   ID_ITEM              INT4                 not null,
-   EMP_ID               INT4                 not null,
-   NAME_ITEM            VARCHAR(255)         null,
-   PRICE_ITEM           FLOAT8               null,
-   COLOR_ITEM           VARCHAR(25)          null,
-   SIZE_ITEM            VARCHAR(5)           null,
-   TYPE_ITEM            VARCHAR(50)          null,
-   STATUS_ITEM          VARCHAR(15)          null,
+   ID_ITEM              VARCHAR(30)          not null,
+   ID_EMP               VARCHAR(10)          not null,
+   NAME_ITEM            VARCHAR(255)         not null,
+   PRICE_ITEM           FLOAT8               not null,
+   COLOR_ITEM           VARCHAR(25)          not null,
+   SIZE_ITEM            VARCHAR(5)           not null,
+   TYPE_ITEM            VARCHAR(50)          not null,
+   STATUS_ITEM          VARCHAR(15)          not null,
    constraint PK_ITEM primary key (ID_ITEM)
 );
 
@@ -120,19 +93,21 @@ ID_ITEM
 /* Index: ADD_ITEM_FK                                           */
 /*==============================================================*/
 create  index ADD_ITEM_FK on ITEM (
-EMP_ID
+ID_EMP
 );
 
 /*==============================================================*/
 /* Table: ORDERLINE                                             */
 /*==============================================================*/
+create sequence orl START 1001;
+
 create table ORDERLINE (
-   ID_ORDERLINE         INT4                 not null,
-   ID_TRANS             INT4                 not null,
-   ID_ITEM              INT4                 not null,
-   ITEM_PRICE           FLOAT8               null,
-   QUANTITY             INT4                 null,
-   SUBTOTAL             FLOAT8               null,
+   ID_ORDERLINE         VARCHAR(15)          DEFAULT 'ORL-' || nextval('orl'),
+   ID_TRANS             VARCHAR(12)          not null,
+   ID_ITEM              VARCHAR(30)          not null,
+   ITEM_PRICE           FLOAT8               not null,
+   QUANTITY             INT4                 not null,
+   SUBTOTAL             FLOAT8               not null,
    constraint PK_ORDERLINE primary key (ID_ORDERLINE)
 );
 
@@ -160,13 +135,15 @@ ID_TRANS
 /*==============================================================*/
 /* Table: OUTCOME                                               */
 /*==============================================================*/
+create sequence outcome START 1001;
+
 create table OUTCOME (
-   ID_OUTCOME           INT4                 not null,
-   ID_ITEM              INT4                 not null,
-   AMOUNT_OUT           FLOAT8               null,
-   QUANTITY_OUT         INT4                 null,
-   TITLE_OUT            DATE                 null,
-   DESC_OUT             VARCHAR(1024)        null,
+   ID_OUTCOME           VARCHAR(10)          DEFAULT 'OUT-' || nextval('outcome'),
+   ID_EMP               VARCHAR(10)          not null,
+   AMOUNT_OUT           FLOAT8               not null,
+   QUANTITY_OUT         INT4                 not null,
+   TITLE_OUT            DATE                 not null,
+   DESC_OUT             VARCHAR(1)           not null,
    constraint PK_OUTCOME primary key (ID_OUTCOME)
 );
 
@@ -178,22 +155,40 @@ ID_OUTCOME
 );
 
 /*==============================================================*/
-/* Index: HAS_OUTCOME_FK                                        */
+/* Index: EMPHASOUTCOME_FK                                      */
 /*==============================================================*/
-create  index HAS_OUTCOME_FK on OUTCOME (
-ID_ITEM
+create  index EMPHASOUTCOME_FK on OUTCOME (
+ID_EMP
+);
+
+/*==============================================================*/
+/* Table: ROLE                                                  */
+/*==============================================================*/
+create table ROLE (
+   ID_ROLE              VARCHAR(10)          SERIAL,
+   ROLE_NAME            VARCHAR(20)          not null,
+   constraint PK_ROLE primary key (ID_ROLE)
+);
+
+/*==============================================================*/
+/* Index: ROLE_PK                                               */
+/*==============================================================*/
+create unique index ROLE_PK on ROLE (
+ID_ROLE
 );
 
 /*==============================================================*/
 /* Table: TRANSACTION                                           */
 /*==============================================================*/
+create sequence trans START 100000000001;
+
 create table TRANSACTION (
-   ID_TRANS             INT4                 not null,
-   ID_DISC              INT4                 not null,
-   EMP_ID               INT4                 not null,
-   CUSTOMER_TRANS       VARCHAR(50)          null,
-   TOTAL_TRANS          FLOAT8               null,
-   DATE_TRANS           DATE                 null,
+   ID_TRANS             VARCHAR(12)          DEFAULT nextval('trans'),
+   ID_DISC              VARCHAR(10)          not null,
+   ID_EMP               VARCHAR(10)          not null,
+   CUSTOMER_TRANS       VARCHAR(50)          not null,
+   TOTAL_TRANS          FLOAT8               not null,
+   DATE_TRANS           DATE                 not null,
    constraint PK_TRANSACTION primary key (ID_TRANS)
 );
 
@@ -215,17 +210,22 @@ ID_DISC
 /* Index: ADD_ORDER_FK                                          */
 /*==============================================================*/
 create  index ADD_ORDER_FK on TRANSACTION (
-EMP_ID
+ID_EMP
 );
 
 alter table DISCOUNT
-   add constraint FK_DISCOUNT_ADD_DISCO_EMPLOYEE foreign key (EMP_ID)
-      references EMPLOYEE (EMP_ID)
+   add constraint FK_DISCOUNT_ADD_DISCO_EMPLOYEE foreign key (ID_EMP)
+      references EMPLOYEE (ID_EMP)
+      on delete restrict on update restrict;
+
+alter table EMPLOYEE
+   add constraint FK_EMPLOYEE_EMPHASROL_ROLE foreign key (ID_ROLE)
+      references ROLE (ID_ROLE)
       on delete restrict on update restrict;
 
 alter table ITEM
-   add constraint FK_ITEM_ADD_ITEM_EMPLOYEE foreign key (EMP_ID)
-      references EMPLOYEE (EMP_ID)
+   add constraint FK_ITEM_ADD_ITEM_EMPLOYEE foreign key (ID_EMP)
+      references EMPLOYEE (ID_EMP)
       on delete restrict on update restrict;
 
 alter table ORDERLINE
@@ -239,13 +239,13 @@ alter table ORDERLINE
       on delete restrict on update restrict;
 
 alter table OUTCOME
-   add constraint FK_OUTCOME_HAS_OUTCO_ITEM foreign key (ID_ITEM)
-      references ITEM (ID_ITEM)
+   add constraint FK_OUTCOME_EMPHASOUT_EMPLOYEE foreign key (ID_EMP)
+      references EMPLOYEE (ID_EMP)
       on delete restrict on update restrict;
 
 alter table TRANSACTION
-   add constraint FK_TRANSACT_ADD_ORDER_EMPLOYEE foreign key (EMP_ID)
-      references EMPLOYEE (EMP_ID)
+   add constraint FK_TRANSACT_ADD_ORDER_EMPLOYEE foreign key (ID_EMP)
+      references EMPLOYEE (ID_EMP)
       on delete restrict on update restrict;
 
 alter table TRANSACTION

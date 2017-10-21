d789b63f6bd5a2df11e6bf06f1f385d904afe8ae
Satya Syahputra <satyadara69@gmail.com>
2017-10-21 17:43:43 +0700

    Menambah Tabel Referensi ITEM
    menambahkan tabel item_color, item_type dan item_merk

--- [1] Document/ERD-DB_Model/SQL/future-sql.sql
+++ [1] Document/ERD-DB_Model/SQL/future-sql.sql
@@ -6,251 +6,356 @@
 /*==============================================================*/
 /* Table: DISCOUNT                                              */
 /*==============================================================*/
-create sequence sec_disc START 101;
-create table DISCOUNT (
-   ID_DISC              VARCHAR(10)	     DEFAULT 'DISC-' || nextval('sec_disc'),
-   ID_EMP               VARCHAR(10)          not null,
-   NAME_DISC            VARCHAR(254)         not null,
-   PERCENTAGE           FLOAT8               not null,
-   START_DATE           DATE                 not null,
-   END_DATE             DATE                 not null,
-   STATUS_DISC          VARCHAR(15)          not null,
-   constraint PK_DISCOUNT primary key (ID_DISC)
+CREATE SEQUENCE sec_disc START 101;
+CREATE TABLE DISCOUNT (
+  ID_DISC     VARCHAR(10) DEFAULT 'DISC-' || nextval('sec_disc'),
+  ID_EMP      VARCHAR(10)  NOT NULL,
+  NAME_DISC   VARCHAR(254) NOT NULL,
+  PERCENTAGE  FLOAT8       NOT NULL,
+  START_DATE  DATE         NOT NULL,
+  END_DATE    DATE         NOT NULL,
+  STATUS_DISC VARCHAR(15)  NOT NULL,
+  CONSTRAINT PK_DISCOUNT PRIMARY KEY (ID_DISC)
 );
 
 /*==============================================================*/
 /* Index: DISCOUNT_PK                                           */
 /*==============================================================*/
-create unique index DISCOUNT_PK on DISCOUNT (
-ID_DISC
-);
+CREATE UNIQUE INDEX DISCOUNT_PK
+  ON DISCOUNT (
+    ID_DISC
+  );
 
 /*==============================================================*/
 /* Index: ADD_DISCOUNT_FK                                       */
 /*==============================================================*/
-create  index ADD_DISCOUNT_FK on DISCOUNT (
-ID_EMP
-);
+CREATE INDEX ADD_DISCOUNT_FK
+  ON DISCOUNT (
+    ID_EMP
+  );
 
 /*==============================================================*/
 /* Table: EMPLOYEE                                              */
 /*==============================================================*/
-create sequence sec_emp START 1001;
+CREATE SEQUENCE sec_emp START 1001;
 
-create table EMPLOYEE (
-   ID_EMP               VARCHAR(10)          DEFAULT 'EMP-' || nextval('sec_emp'),
-   ID_ROLE              INT                  not null,
-   USERNAME             VARCHAR(15)          not null,
-   PASSWORD             VARCHAR(64)          not null,
-   NOKTP                VARCHAR(35)          not null,
-   FULL_NAME            VARCHAR(60)          not null,
-   ADDRESS              VARCHAR(256)         not null,
-   GENDER               CHAR(1)              not null,
-   TELEPHONE            VARCHAR(15)          not null,
-   STATUS_EMPLOYEE      VARCHAR(15)          not null,
-   constraint PK_EMPLOYEE primary key (ID_EMP)
+CREATE TABLE EMPLOYEE (
+  ID_EMP          VARCHAR(10) DEFAULT 'EMP-' || nextval('sec_emp'),
+  ID_ROLE         INT          NOT NULL,
+  USERNAME        VARCHAR(15)  NOT NULL,
+  PASSWORD        VARCHAR(64)  NOT NULL,
+  NOKTP           VARCHAR(35)  NOT NULL,
+  FULL_NAME       VARCHAR(60)  NOT NULL,
+  ADDRESS         VARCHAR(256) NOT NULL,
+  GENDER          CHAR(1)      NOT NULL,
+  TELEPHONE       VARCHAR(15)  NOT NULL,
+  STATUS_EMPLOYEE VARCHAR(15)  NOT NULL,
+  CONSTRAINT PK_EMPLOYEE PRIMARY KEY (ID_EMP)
 );
 
 /*==============================================================*/
 /* Index: EMPLOYEE_PK                                           */
 /*==============================================================*/
-create unique index EMPLOYEE_PK on EMPLOYEE (
-ID_EMP
-);
+CREATE UNIQUE INDEX EMPLOYEE_PK
+  ON EMPLOYEE (
+    ID_EMP
+  );
 
 /*==============================================================*/
 /* Index: EMPHASROLE_FK                                         */
 /*==============================================================*/
-create  index EMPHASROLE_FK on EMPLOYEE (
-ID_ROLE
-);
+CREATE INDEX EMPHASROLE_FK
+  ON EMPLOYEE (
+    ID_ROLE
+  );
 
 /*==============================================================*/
 /* Table: ITEM                                                  */
 /*==============================================================*/
-create sequence sec_item START 101;
+CREATE SEQUENCE sec_item START 101;
 
-create table ITEM (
-   ID_ITEM              VARCHAR(30)          not null,
-   ID_EMP               VARCHAR(10)          not null,
-   NAME_ITEM            VARCHAR(255)         not null,
-   PRICE_ITEM           FLOAT8               not null,
-   STOCK_ITEM           INT		     not null,
-   COLOR_ITEM           VARCHAR(25)          not null,
-   SIZE_ITEM            VARCHAR(5)           not null,
-   TYPE_ITEM            VARCHAR(50)          not null,
-   STATUS_ITEM          VARCHAR(15)          not null,
-   constraint PK_ITEM primary key (ID_ITEM)
+CREATE TABLE ITEM (
+  ID_ITEM     VARCHAR(30)  NOT NULL,
+  ID_EMP      VARCHAR(10)  NOT NULL,
+  NAME_ITEM   VARCHAR(255) NOT NULL,
+  PRICE_ITEM  FLOAT8       NOT NULL,
+  STOCK_ITEM  INT4         NOT NULL,
+  MERK_ITEM   VARCHAR(3)   NOT NULL,
+  COLOR_ITEM  VARCHAR(3)   NOT NULL,
+  SIZE_ITEM   VARCHAR(5)   NOT NULL,
+  TYPE_ITEM   VARCHAR(3)   NOT NULL,
+  STATUS_ITEM VARCHAR(15)  NOT NULL,
+  CONSTRAINT PK_ITEM PRIMARY KEY (ID_ITEM)
 );
 
 /*==============================================================*/
 /* Index: ITEM_PK                                               */
 /*==============================================================*/
-create unique index ITEM_PK on ITEM (
-ID_ITEM
-);
+CREATE UNIQUE INDEX ITEM_PK
+  ON ITEM (
+    ID_ITEM
+  );
 
 /*==============================================================*/
 /* Index: ADD_ITEM_FK                                           */
 /*==============================================================*/
-create  index ADD_ITEM_FK on ITEM (
-ID_EMP
-);
+CREATE INDEX ADD_ITEM_FK
+  ON ITEM (
+    ID_EMP
+  );
 
 /*==============================================================*/
 /* Table: ORDERLINE                                             */
 /*==============================================================*/
-create sequence sec_orl START 100000000000001;
+CREATE SEQUENCE sec_orl START 100000000000001;
 
-create table ORDERLINE (
-   ID_ORDERLINE         VARCHAR(15)          DEFAULT 'ORL-' || nextval('sec_orl'),
-   ID_TRANS             VARCHAR(12)          not null,
-   ID_ITEM              VARCHAR(30)          not null,
-   ITEM_PRICE           FLOAT8               not null,
-   QUANTITY             INT4                 not null,
-   SUBTOTAL             FLOAT8               not null,
-   constraint PK_ORDERLINE primary key (ID_ORDERLINE)
+CREATE TABLE ORDERLINE (
+  ID_ORDERLINE VARCHAR(15) DEFAULT 'ORL-' || nextval('sec_orl'),
+  ID_TRANS     VARCHAR(12) NOT NULL,
+  ID_ITEM      VARCHAR(30) NOT NULL,
+  ITEM_PRICE   FLOAT8      NOT NULL,
+  QUANTITY     INT4        NOT NULL,
+  SUBTOTAL     FLOAT8      NOT NULL,
+  CONSTRAINT PK_ORDERLINE PRIMARY KEY (ID_ORDERLINE)
 );
 
 /*==============================================================*/
 /* Index: ORDERLINE_PK                                          */
 /*==============================================================*/
-create unique index ORDERLINE_PK on ORDERLINE (
-ID_ORDERLINE
-);
+CREATE UNIQUE INDEX ORDERLINE_PK
+  ON ORDERLINE (
+    ID_ORDERLINE
+  );
 
 /*==============================================================*/
 /* Index: ITEMHASORDERLINE_FK                                   */
 /*==============================================================*/
-create  index ITEMHASORDERLINE_FK on ORDERLINE (
-ID_ITEM
-);
+CREATE INDEX ITEMHASORDERLINE_FK
+  ON ORDERLINE (
+    ID_ITEM
+  );
 
 /*==============================================================*/
 /* Index: ORDERHASORDERLINE_FK                                  */
 /*==============================================================*/
-create  index ORDERHASORDERLINE_FK on ORDERLINE (
-ID_TRANS
-);
+CREATE INDEX ORDERHASORDERLINE_FK
+  ON ORDERLINE (
+    ID_TRANS
+  );
 
 /*==============================================================*/
 /* Table: OUTCOME                                               */
 /*==============================================================*/
-create sequence sec_outcome START 101;
+CREATE SEQUENCE sec_outcome START 101;
 
-create table OUTCOME (
-   ID_OUTCOME           VARCHAR(10)          DEFAULT 'OUT-' || nextval('sec_outcome'),
-   ID_EMP               VARCHAR(10)          not null,
-   AMOUNT_OUT           FLOAT8               not null,
-   QUANTITY_OUT         INT4                 not null,
-   TITLE_OUT            DATE                 not null,
-   DESC_OUT             VARCHAR(1)           not null,
-   constraint PK_OUTCOME primary key (ID_OUTCOME)
+CREATE TABLE OUTCOME (
+  ID_OUTCOME   VARCHAR(10) DEFAULT 'OUT-' || nextval('sec_outcome'),
+  ID_EMP       VARCHAR(10) NOT NULL,
+  AMOUNT_OUT   FLOAT8      NOT NULL,
+  QUANTITY_OUT INT4        NOT NULL,
+  TITLE_OUT    DATE        NOT NULL,
+  DESC_OUT     VARCHAR(1)  NOT NULL,
+  CONSTRAINT PK_OUTCOME PRIMARY KEY (ID_OUTCOME)
 );
 
 /*==============================================================*/
 /* Index: OUTCOME_PK                                            */
 /*==============================================================*/
-create unique index OUTCOME_PK on OUTCOME (
-ID_OUTCOME
-);
+CREATE UNIQUE INDEX OUTCOME_PK
+  ON OUTCOME (
+    ID_OUTCOME
+  );
 
 /*==============================================================*/
 /* Index: EMPHASOUTCOME_FK                                      */
 /*==============================================================*/
-create  index EMPHASOUTCOME_FK on OUTCOME (
-ID_EMP
-);
+CREATE INDEX EMPHASOUTCOME_FK
+  ON OUTCOME (
+    ID_EMP
+  );
 
 /*==============================================================*/
 /* Table: ROLE                                                  */
 /*==============================================================*/
-create table ROLE (
-   ID_ROLE              SERIAL,
-   ROLE_NAME            VARCHAR(20)          not null,
-   constraint PK_ROLE primary key (ID_ROLE)
+CREATE TABLE ROLE (
+  ID_ROLE   SERIAL,
+  ROLE_NAME VARCHAR(20) NOT NULL,
+  CONSTRAINT PK_ROLE PRIMARY KEY (ID_ROLE)
 );
 
 /*==============================================================*/
 /* Index: ROLE_PK                                               */
 /*==============================================================*/
-create unique index ROLE_PK on ROLE (
-ID_ROLE
-);
+CREATE UNIQUE INDEX ROLE_PK
+  ON ROLE (
+    ID_ROLE
+  );
 
 /*==============================================================*/
 /* Table: TRANSACTION                                           */
 /*==============================================================*/
-create sequence sec_trans START 100000000001;
+CREATE SEQUENCE sec_trans START 100000000001;
 
-create table TRANSACTION (
-   ID_TRANS             VARCHAR(12)          DEFAULT nextval('sec_trans'),
-   ID_DISC              VARCHAR(10)          not null,
-   ID_EMP               VARCHAR(10)          not null,
-   CUSTOMER_TRANS       VARCHAR(50)          not null,
-   TOTAL_TRANS          FLOAT8               not null,
-   DATE_TRANS           DATE                 not null,
-   constraint PK_TRANSACTION primary key (ID_TRANS)
+CREATE TABLE TRANSACTION (
+  ID_TRANS       VARCHAR(12) DEFAULT nextval('sec_trans'),
+  ID_DISC        VARCHAR(10) NOT NULL,
+  ID_EMP         VARCHAR(10) NOT NULL,
+  CUSTOMER_TRANS VARCHAR(50) NOT NULL,
+  TOTAL_TRANS    FLOAT8      NOT NULL,
+  DATE_TRANS     DATE        NOT NULL,
+  CONSTRAINT PK_TRANSACTION PRIMARY KEY (ID_TRANS)
 );
 
 /*==============================================================*/
 /* Index: TRANSACTION_PK                                        */
 /*==============================================================*/
-create unique index TRANSACTION_PK on TRANSACTION (
-ID_TRANS
+CREATE UNIQUE INDEX TRANSACTION_PK
+  ON TRANSACTION (
+    ID_TRANS
+  );
+
+/*==============================================================*/
+/* Table: ITEM_COLOR                                            */
+/*==============================================================*/
+CREATE TABLE ITEM_COLOR (
+  ID_ITEM_COLOR   VARCHAR(3)  NOT NULL,
+  NAME_ITEM_COLOR VARCHAR(25) NOT NULL,
+  CONSTRAINT PK_ITEM_COLOR PRIMARY KEY (ID_ITEM_COLOR)
 );
+
+/*==============================================================*/
+/* Index: ITEM_COLOR_PK                                         */
+/*==============================================================*/
+CREATE UNIQUE INDEX ITEM_COLOR_PK
+  ON ITEM_COLOR (
+    ID_ITEM_COLOR
+  );
+
+/*==============================================================*/
+/* Table: ITEM_TYPE                                             */
+/*==============================================================*/
+CREATE TABLE ITEM_TYPE (
+  ID_ITEM_TYPE   VARCHAR(3)  NOT NULL,
+  NAME_ITEM_TYPE VARCHAR(25) NOT NULL,
+  CONSTRAINT PK_ITEM_TYPE PRIMARY KEY (ID_ITEM_TYPE)
+);
+
+/*==============================================================*/
+/* Index: ITEM_TYPE_PK                                          */
+/*==============================================================*/
+CREATE UNIQUE INDEX ITEM_TYPE_PK
+  ON ITEM_TYPE (
+    ID_ITEM_TYPE
+  );
+
+/*==============================================================*/
+/* Table: ITEM_MERK                                             */
+/*==============================================================*/
+CREATE TABLE ITEM_MERK (
+  ID_ITEM_MERK   VARCHAR(3)  NOT NULL,
+  NAME_ITEM_MERK VARCHAR(25) NOT NULL,
+  CONSTRAINT PK_ITEM_MERK PRIMARY KEY (ID_ITEM_MERK)
+);
+
+/*==============================================================*/
+/* Index: ITEM_MERK_PK                                         */
+/*==============================================================*/
+CREATE UNIQUE INDEX ITEM_MERK_PK
+  ON ITEM_MERK (
+    ID_ITEM_MERK
+  );
 
 /*==============================================================*/
 /* Index: HAS_FK                                                */
 /*==============================================================*/
-create  index HAS_FK on TRANSACTION (
-ID_DISC
-);
+CREATE INDEX HAS_FK
+  ON TRANSACTION (
+    ID_DISC
+  );
 
 /*==============================================================*/
 /* Index: ADD_ORDER_FK                                          */
 /*==============================================================*/
-create  index ADD_ORDER_FK on TRANSACTION (
-ID_EMP
-);
+CREATE INDEX ADD_ORDER_FK
+  ON TRANSACTION (
+    ID_EMP
+  );
 
-alter table DISCOUNT
-   add constraint FK_DISCOUNT_ADD_DISCO_EMPLOYEE foreign key (ID_EMP)
-      references EMPLOYEE (ID_EMP)
-      on delete restrict on update restrict;
+/*==============================================================*/
+/* Index: ITEM_COLOR_FK                                         */
+/*==============================================================*/
+CREATE INDEX ITEM_COLOR_FK
+  ON ITEM (
+    COLOR_ITEM
+  );
 
-alter table EMPLOYEE
-   add constraint FK_EMPLOYEE_EMPHASROL_ROLE foreign key (ID_ROLE)
-      references ROLE (ID_ROLE)
-      on delete restrict on update restrict;
+/*==============================================================*/
+/* Index: ITEM_TYPE_FK                                          */
+/*==============================================================*/
+CREATE INDEX ITEM_TYPE_FK
+  ON ITEM (
+    TYPE_ITEM
+  );
 
-alter table ITEM
-   add constraint FK_ITEM_ADD_ITEM_EMPLOYEE foreign key (ID_EMP)
-      references EMPLOYEE (ID_EMP)
-      on delete restrict on update restrict;
+/*==============================================================*/
+/* Index: ITEM_MERK_FK                                          */
+/*==============================================================*/
+CREATE INDEX ITEM_MERK_FK
+  ON ITEM (
+    MERK_ITEM
+  );
 
-alter table ORDERLINE
-   add constraint FK_ORDERLIN_ITEMHASOR_ITEM foreign key (ID_ITEM)
-      references ITEM (ID_ITEM)
-      on delete restrict on update restrict;
+ALTER TABLE DISCOUNT
+  ADD CONSTRAINT FK_DISCOUNT_ADD_DISCO_EMPLOYEE FOREIGN KEY (ID_EMP)
+REFERENCES EMPLOYEE (ID_EMP)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-alter table ORDERLINE
-   add constraint FK_ORDERLIN_ORDERHASO_TRANSACT foreign key (ID_TRANS)
-      references TRANSACTION (ID_TRANS)
-      on delete restrict on update restrict;
+ALTER TABLE EMPLOYEE
+  ADD CONSTRAINT FK_EMPLOYEE_EMPHASROL_ROLE FOREIGN KEY (ID_ROLE)
+REFERENCES ROLE (ID_ROLE)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-alter table OUTCOME
-   add constraint FK_OUTCOME_EMPHASOUT_EMPLOYEE foreign key (ID_EMP)
-      references EMPLOYEE (ID_EMP)
-      on delete restrict on update restrict;
+ALTER TABLE ITEM
+  ADD CONSTRAINT FK_ITEM_ADD_ITEM_EMPLOYEE FOREIGN KEY (ID_EMP)
+REFERENCES EMPLOYEE (ID_EMP)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-alter table TRANSACTION
-   add constraint FK_TRANSACT_ADD_ORDER_EMPLOYEE foreign key (ID_EMP)
-      references EMPLOYEE (ID_EMP)
-      on delete restrict on update restrict;
+ALTER TABLE ITEM
+  ADD CONSTRAINT FK_ITEM_COLOR FOREIGN KEY (COLOR_ITEM)
+REFERENCES ITEM_COLOR (ID_ITEM_COLOR)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-alter table TRANSACTION
-   add constraint FK_TRANSACT_HAS_DISCOUNT foreign key (ID_DISC)
-      references DISCOUNT (ID_DISC)
-      on delete restrict on update restrict;
+ALTER TABLE ITEM
+  ADD CONSTRAINT FK_ITEM_TYPE FOREIGN KEY (TYPE_ITEM)
+REFERENCES ITEM_TYPE (ID_ITEM_TYPE)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
 
+ALTER TABLE ITEM
+  ADD CONSTRAINT FK_ITEM_MERK FOREIGN KEY (MERK_ITEM)
+REFERENCES ITEM_MERK (ID_ITEM_MERK)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE ORDERLINE
+  ADD CONSTRAINT FK_ORDERLIN_ITEMHASOR_ITEM FOREIGN KEY (ID_ITEM)
+REFERENCES ITEM (ID_ITEM)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE ORDERLINE
+  ADD CONSTRAINT FK_ORDERLIN_ORDERHASO_TRANSACT FOREIGN KEY (ID_TRANS)
+REFERENCES TRANSACTION (ID_TRANS)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE OUTCOME
+  ADD CONSTRAINT FK_OUTCOME_EMPHASOUT_EMPLOYEE FOREIGN KEY (ID_EMP)
+REFERENCES EMPLOYEE (ID_EMP)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE TRANSACTION
+  ADD CONSTRAINT FK_TRANSACT_ADD_ORDER_EMPLOYEE FOREIGN KEY (ID_EMP)
+REFERENCES EMPLOYEE (ID_EMP)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE TRANSACTION
+  ADD CONSTRAINT FK_TRANSACT_HAS_DISCOUNT FOREIGN KEY (ID_DISC)
+REFERENCES DISCOUNT (ID_DISC)
+ON DELETE RESTRICT ON UPDATE RESTRICT;

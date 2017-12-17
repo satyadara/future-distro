22a2229d6db06c62380c3f23b86a1c4c789fe03a
Raditia Madya <gustiraditia@gmail.com>
2017-12-17 23:11:20 +0700

    Add new class

---
+++ [2] Project Implementation/src/main/java/com/blibli/distro_pos/WebInitializer.java
@@ -0,0 +1,25 @@
+package com.blibli.distro_pos;
+
+import com.blibli.distro_pos.Config.WebMvcConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+    private int maxUploadSizeInMb = 5 * 1024 * 1024; // 5 MB
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebMvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+}
